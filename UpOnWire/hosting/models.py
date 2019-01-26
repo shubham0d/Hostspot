ab3803f4c747d28bcf9a5e8f5b6c03e1ca1343e6
Shubham Dubey <sdubey504@gmail.com>
2019-01-26 21:20:24 +0530

    added model

--- UpOnWire/hosting/models.py
+++ UpOnWire/hosting/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class DefaultConf(models.Model):
+    WHAT_TO_HOST = (
+        ('W', 'Website'),
+        ('V', 'Video'),
+        ('I', 'Image'),
+        ('O', 'Other'),
+    )
+    imageId = models.CharField(max_length=1000, primary_key=True)
+    creationDate = models.DateField()
+    url = models.CharField(max_length=1000)
+    expireDate = models.IntegerField()
+    hostingType = models.CharField(max_length=1, choices=WHAT_TO_HOST)
+    active = models.BooleanField(default=False)
